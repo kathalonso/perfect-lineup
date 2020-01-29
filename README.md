61bcabc3c766d5fee5f811dbb33590c01471c1d4
John Carmichael <jc@stackeducation.com>
2020-01-29 17:21:15 -0500

    Initial commit of files provided to the students

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Perfect Lineup
+
+## Instructions
+In the initial commit to this project, you have been provided with files that contain a JavaScript module and some associated tests. Your task is to implement a `validateLineup` function that will return `true` if the lineup provided adheres to all of the rules listed below.
+
+## Lineup Rules
+1) The total salary of all players in a lineup may not exceed $45,000
+2) Lineups may not contain more than 2 players from a single team
+3) Lineups may not contain more than 3 players from a single game
+4) Lineups must contain exactly 3 players with the position of 'OF' and must also contain exactly 1 player from each of the following positions: 'P', 'C', '1B', '2B', '3B', 'SS'
---
+++ tests.js
@@ -0,0 +1,195 @@
+const expect = require('chai').expect
+const { describe, it } = require('mocha')
+const validateLineup = require('./index')
+
+describe('validateLineup', () => {
+  it('returns true when the lineup satisfies all conditions', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+    }, {
+      id: 3, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 5, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }, {
+      id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+    }]
+
+    expect(validateLineup(lineup)).to.equal(true)
+  })
+
+  it('returns false when the lineup includes too many players from a single team', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+    }, {
+      id: 3, name: 'Mitch Moreland', position: '1B', teamId: 12, gameId: 123, salary: 2800
+    }, {
+      id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 5, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }, {
+      id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup includes too many players from a single game', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 123, salary: 3800
+    }, {
+      id: 3, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 123, salary: 2500
+    }, {
+      id: 4, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 5, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 6, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 7, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 8, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup includes too many players', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+    }, {
+      id: 3, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 4, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 5, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }, {
+      id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+    }, {
+      id: 10, name: 'Buster Posey', position: 'C', teamId: 21, gameId: 124, salary: 4000
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup does not have the right number of players at each position', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+    }, {
+      id: 3, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 4, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 5, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }, {
+      id: 9, name: 'Buster Posey', position: 'C', teamId: 21, gameId: 124, salary: 4000
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup includes too few players from a single position', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 2, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 3, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 2500
+    }, {
+      id: 4, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 5, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 6, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup does not include a player from a position', () => {
+    const lineup = [{
+      id: 1, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 2800
+    }, {
+      id: 2, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 3200
+    }, {
+      id: 3, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 3100
+    }, {
+      id: 4, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 9500
+    }, {
+      id: 5, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 3300
+    }, {
+      id: 6, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 3800
+    }, {
+      id: 7, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 3800
+    }, {
+      id: 8, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 3600
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+
+  it('returns false when the lineup has a total salary greater than 45000', () => {
+    const lineup = [{
+      id: 1, name: 'Chris Sale', position: 'P', teamId: 12, gameId: 123, salary: 10800
+    }, {
+      id: 2, name: 'Yadier Molina', position: 'C', teamId: 22, gameId: 115, salary: 4000
+    }, {
+      id: 3, name: 'Luke Voit', position: '1B', teamId: 20, gameId: 115, salary: 4400
+    }, {
+      id: 4, name: 'Dee Gordon', position: '2B', teamId: 18, gameId: 101, salary: 4500
+    }, {
+      id: 5, name: 'Manny Machado', position: '3B', teamId: 14, gameId: 134, salary: 4100
+    }, {
+      id: 6, name: 'Troy Tulowitzki', position: 'SS', teamId: 27, gameId: 126, salary: 4500
+    }, {
+      id: 7, name: 'Andrew McCutchen', position: 'OF', teamId: 11, gameId: 131, salary: 4200
+    }, {
+      id: 8, name: 'Bryce Harper', position: 'OF', teamId: 15, gameId: 119, salary: 4400
+    }, {
+      id: 9, name: 'Mookie Betts', position: 'OF', teamId: 12, gameId: 123, salary: 4200
+    }]
+
+    expect(validateLineup(lineup)).to.equal(false)
+  })
+})
